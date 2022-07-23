e30ea56a88099350c2e641c34d8f0ccf295dfbcb
Tushar <tusharwalzade216@gmail.com>
2022-07-23 20:43:24 +0530

    week5/assignment4: updated test cases for invalid inputs

--- Week-5/Assignment-4/src/calculator.js
+++ Week-5/Assignment-4/src/calculator.js
@@ -1,12 +1,23 @@
 const mathOperations = {
+    validateInput: function (a, b) {
+        return typeof a === "number" && typeof b === "number"
+            && !isNaN(a) && !isNaN(b)
+            && Number.isFinite(a) && Number.isFinite(b);
+    },
     sum: function (a, b) {
-        return a + b;
+        if (this.validateInput(a, b))
+            return a + b;
+        throw new TypeError('Invalid inputs provided');
     },
     diff: function (a, b) {
-        return a - b;
+        if (this.validateInput(a, b))
+            return a - b;
+        throw new TypeError('Invalid inputs provided');
     },
     product: function (a, b) {
-        return a * b;
+        if (this.validateInput(a, b))
+            return a * b;
+        throw new TypeError('Invalid inputs provided');
     }
 }
 
