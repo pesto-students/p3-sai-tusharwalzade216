3f8c5c76621ef53690e7f360d6e015543b5aa2ea
Tushar <tusharwalzade216@gmail.com>
2022-07-27 00:10:33 +0530

    week6/assignment2: error handling

--- Week-6/Assignment-2/index.js
+++ Week-6/Assignment-2/index.js
@@ -1,4 +1,4 @@
-const { generateMatrix, displayMatrix } = require("./utils");
+const { generateMatrix, displayMatrix, validateInput } = require("./utils");
 
 /**
  * @function spirallyTraverse
@@ -7,6 +7,8 @@ const { generateMatrix, displayMatrix } = require("./utils");
  * @returns a new one dimensional array with spiral order of elements
  */
 function spirallyTraverse(matrix) {
+    if (!validateInput(matrix)) throw new Error('Invalid input provided');
+
     // # rows & # columns
     const rows = matrix.length, cols = matrix?.[0]?.length || 0;
     // result array & iterator
@@ -56,45 +58,81 @@ function spirallyTraverse(matrix) {
     return arr;
 }
 
-const mat33 = generateMatrix(3, 3);
-console.log(displayMatrix(mat33));
-console.log(spirallyTraverse(mat33));
+try {
+    const mat33 = generateMatrix(3, 3);
+    console.log(displayMatrix(mat33));
+    /**
+     * Sample Output
+     * "1,2,3
+     * 4,5,6
+     * 7,8,9"
+     */
+    console.log(spirallyTraverse(mat33));
+    //  [1, 2, 3, 6, 9, 8, 7, 4, 5]
+} catch (error) {
+    console.error(error)
+}
 
-/**
- * Sample Output
- * "1,2,3
- * 4,5,6
- * 7,8,9"
- * [1, 2, 3, 6, 9, 8, 7, 4, 5]
- */
+try {
+    const mat44 = generateMatrix(4, 4);
+    console.log(displayMatrix(mat44));
+    /**
+     * Sample Output
+     * "1,2,3,4
+     * 5,6,7,8
+     * 9,10,11,12
+     * 13,14,15,16"
+     */
+    console.log(spirallyTraverse(mat44));
+    //  [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+} catch (error) {
+    console.error(error);
+}
 
-const mat44 = generateMatrix(4, 4);
-console.log(displayMatrix(mat44));
-console.log(spirallyTraverse(mat44));
+try {
+    const mat99 = generateMatrix(9, 9);
+    console.log(displayMatrix(mat99));
+    /**
+     * Sample Output
+     * "1,2,3,4,5,6,7,8,9
+     * 10,11,12,13,14,15,16,17,18
+     * 19,20,21,22,23,24,25,26,27
+     * 28,29,30,31,32,33,34,35,36
+     * 37,38,39,40,41,42,43,44,45
+     * 46,47,48,49,50,51,52,53,54
+     * 55,56,57,58,59,60,61,62,63
+     * 64,65,66,67,68,69,70,71,72
+     * 73,74,75,76,77,78,79,80,81"
+     */
+    console.log(spirallyTraverse(mat99));
+    /**
+     * [1, 2, 3, 4, 5, 6, 7, 8, 9, 18, 27, 36, 45, 54, 63, 72, 81, 80, 79, 78, 77, 76, 75, 74, 
+     * 73, 64, 55, 46, 37, 28, 19, 10, 11, 12, 13, 14, 15, 16, 17, 26, 35, 44, 53, 62, 71, 70, 
+     * 69, 68, 67, 66, 65, 56, 47, 38, 29, 20, 21, 22, 23, 24, 25, 34, 43, 52, 61, 60, 59, 58, 
+     * 57, 48, 39, 30, 31, 32, 33, 42, 51, 50, 49, 40, 41]
+     */
+} catch (error) {
+    console.error(error);
+}
 
-/**
- * Sample Output
- * "1,2,3,4
- * 5,6,7,8
- * 9,10,11,12
- * 13,14,15,16"
- * [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
- */
+try {
+    const mat11 = generateMatrix(1, 1);
+    console.log(displayMatrix(mat11));
+    /**
+     * Sample Output
+     * "1"
+     */
+    console.log(spirallyTraverse(mat11));
+    //  [1]
+} catch (error) {
+    console.error(error);
+}
 
-const mat99 = generateMatrix(9, 9);
-console.log(displayMatrix(mat99));
-console.log(spirallyTraverse(mat99));
-
-/**
- * Sample Output
- * "1,2,3,4,5,6,7,8,9
- * 10,11,12,13,14,15,16,17,18
- * 19,20,21,22,23,24,25,26,27
- * 28,29,30,31,32,33,34,35,36
- * 37,38,39,40,41,42,43,44,45
- * 46,47,48,49,50,51,52,53,54
- * 55,56,57,58,59,60,61,62,63
- * 64,65,66,67,68,69,70,71,72
- * 73,74,75,76,77,78,79,80,81"
- * [1, 2, 3, 4, 5, 6, 7, 8, 9, 18, 27, 36, 45, 54, 63, 72, 81, 80, 79, 78, 77, 76, 75, 74, 73, 64, 55, 46, 37, 28, 19, 10, 11, 12, 13, 14, 15, 16, 17, 26, 35, 44, 53, 62, 71, 70, 69, 68, 67, 66, 65, 56, 47, 38, 29, 20, 21, 22, 23, 24, 25, 34, 43, 52, 61, 60, 59, 58, 57, 48, 39, 30, 31, 32, 33, 42, 51, 50, 49, 40, 41]
- */
+try {
+    const mat00 = generateMatrix(0, 0);
+    console.log(displayMatrix(mat00));
+    console.log(spirallyTraverse(mat00));
+    //  Error: Invalid input provided
+} catch (error) {
+    console.error(error);
+}
--- Week-6/Assignment-2/utils/index.js
+++ Week-6/Assignment-2/utils/index.js
@@ -4,7 +4,7 @@
  * @param {Number} cols number of cols to generate 
  * @returns a matrix generated
  */
- const generateMatrix = (rows, cols) => {
+const generateMatrix = (rows, cols) => {
     let matrix = [], row, last = 0;
     for (let i = 0; i < rows; i++) {
         row = [];
@@ -21,7 +21,16 @@
  */
 const displayMatrix = matrix => matrix.join('\n');
 
+/**
+ * @function validateInput checks if the input is valid 2-D array
+ * @param {Array} arr an array
+ * @returns boolean
+ */
+const validateInput = (arr) => Array.isArray(arr) && arr.length > 0 &&
+    arr.every(el => Array.isArray(el) && el?.length > 0);
+
 module.exports = {
     generateMatrix,
-    displayMatrix
+    displayMatrix,
+    validateInput
 }
